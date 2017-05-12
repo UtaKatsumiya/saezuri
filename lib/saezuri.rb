67760ac1247f1639929c2cfd77f644732313b8c3
UtaKatsumiya <grimoire.sic@gmail.com>
2017-05-12 20:43:05 +0900

    split

--- lib/saezuri.rb
+++ lib/saezuri.rb
@@ -3,52 +3,16 @@
 
 Dir[File.dirname(__FILE__) + '/sub/*.rb'].each {|file| require file }
 
-require "twitter"
-require "optparse"
 
+cmd = Cmd.new
+argument = Argument.new
 
-opt = OptionParser.new
-options = {}
-opt.on("-u", "--user=TwitterID", "TwitterID"){|v| options[:twitterid] = v}
-opt.on("-s", "--sname=ScreanName", "ScreanName"){|v| options[:screanname] = v}
-opt.on("-n", "--number=TweetID", "status_id"){|v| options[:tweetid] = v}
-opt.on("-t", "--tweet=TweetText", "TweetText"){|v| options[:text] = v}
-opt.on("-i", "--image=ImageFile", "ImageFile"){|v| options[:image] = v}
-opt.on("-c", "--count=FetchCounts", "FetchCount"){|v| options[:fetchcount] = v}
-opt.on("-f"){|v| options[:favflag] = v}
-opt.on("-r"){|v| options[:rtflag] = v}
-opt.on("-w"){|v| options[:watchflag] = v}
-opt.parse!(ARGV)
-
-twitteruser = options[:twitterid]
-twitteruser ||= "default_user"
-@twitteruser = twitteruser
-
-@screen_name = options[:screanname]
-
-@tweet_id = options[:tweetid]
-
-tweettext = options[:text]
-tweettext ||= ARGV[0]
-@tweettext = tweettext
-
-@image_directory = options[:image]
-
-fetch_count = options[:fetchcount]
-fetch_count ||= 5
-@fetch_count = fetch_count
-
-@fav = options[:favflag]
-@rt = options[:rtflag]
-
-@watch = options[:watchflag]
-
-if @fav || @rt
-  favrt
-elsif @screen_name || @watch
-  show_timeline
-elsif @tweettext
-  tweet
+if argument.rt || argument.fav
+  cmd.favrt
+elsif argument.watch || argument.sname
+  cmd.show_timeline
+elsif argument.text
+  cmd.tweet
 else
-  msg_check
+  cmd.msg_check
 end
