8068ec1a3c04f948f15435e8e9988a40ff3a5890
UtaKatsumiya <grimoire.sic@gmail.com>
2017-05-12 20:51:17 +0900

    first commit

---
+++ lib/sub/cmd.rb
@@ -0,0 +1,60 @@
+#!/usr/bin/env ruby
+# coding: windows-31j
+
+
+class Cmd
+  def initialize
+    @argument = Argument.new
+    token = Token.new(@argument.twitterid)
+    @modified = token.login
+  end
+
+  def tweet
+    if @argument.image && @argument.tweetid
+      @modified.update_with_media(@argument.text.dup.encode!("UTF-8"), open(@argument.image), in_reply_to_status_id: @argument.tweetid)
+    else
+      @modified.update(@argument.text.dup.encode!("UTF-8"), in_reply_to_status_id: @argument.tweetid)
+   end
+  end
+
+  def show_timeline
+    unless @argument.sname
+      max_id = @modified.home_timeline.first.id
+      @modified.home_timeline(max_id: max_id, count: @argument.fetchcount).each do |tweet|
+        puts "#{tweet.user.name} @#{tweet.user.screen_name} (id #{tweet.id})"
+        puts tweet.full_text
+        puts "Fav: #{tweet.favorite_count}, RT: #{tweet.retweet_count} posted at #{tweet.created_at}"
+        puts ""
+      end
+    else
+      @modified.user_timeline(@argument.sname, { count: @argument.fetchcount} ).each do |timeline|
+      tweet = @modified.status(timeline.id)
+        puts "#{tweet.user.name} @#{tweet.user.screen_name} (id #{tweet.id})"
+        puts tweet.text
+        puts "Fav: #{tweet.favorite_count}, RT: #{tweet.retweet_count} posted at #{tweet.created_at}"
+        puts ""
+      end
+    end
+  end
+
+  def msg_check
+    mention_max_id = @modified.mentions_timeline.first.id
+    @modified.mentions_timeline(max_id: mention_max_id, count: @argument.fetchcount).each do |tweet|
+      puts "#{tweet.user.name} @#{tweet.user.screen_name} (id #{tweet.id})"
+      puts tweet.full_text
+      puts "Fav: #{tweet.favorite_count}, RT: #{tweet.retweet_count} posted at #{tweet.created_at}"
+      puts ""
+    end
+  end
+
+  def favrt
+    if @argument.fav && @argument.rt
+      @modified.favorite(@argument.tweetid)
+      @modified.retweet(@argument.tweetid)
+    elsif @argument.fav
+      @modified.favorite(@argument.tweetid)
+    elsif @argument.rt
+      @modified.retweet(@argument.tweetid)
+    end
+  end
+end
