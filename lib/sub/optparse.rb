38e8b41744249edc42135fac4e5507e337e7eae1
UtaKatsumiya <grimoire.sic@gmail.com>
2017-05-13 19:41:58 +0900

    text → posttext

--- lib/sub/optparse.rb
+++ lib/sub/optparse.rb
@@ -4,16 +4,16 @@
 require "optparse"
 
 class Argument
-  attr_accessor :twitterid, :sname, :tweetid, :text, :image, :fetchcount, :fav, :rt, :watch
+  attr_accessor :twitterid, :sname, :tweetid, :posttext, :image, :fetchcount, :fav, :rt, :watch
 
   def initialize
     opt = OptionParser.new
-    options = {:twitterid => "default_user", :fetchcount => 5}
+    options = {:twitterid => "Uta_Katsumiya", :fetchcount => 5}
 
     opt.on("-u", "--user=TwitterID", "TwitterID"){|v| options[:twitterid] = v}
     opt.on("-s", "--sname=ScreenName", "ScreenName"){|v| options[:sname] = v}
     opt.on("-n", "--number=TweetID", "status_id"){|v| options[:tweetid] = v}
-    opt.on("-t", "--tweet=TweetText", "TweetText"){|v| options[:text] = v}
+    opt.on("-t", "--tweet=TweetText", "TweetText"){|v| options[:posttext] = v}
     opt.on("-i", "--image=ImageFile", "ImageFile"){|v| options[:image] = v}
     opt.on("-c", "--count=FetchCounts", "FetchCount"){|v| options[:fetchcount] = v}
     opt.on("-f"){|v| options[:fav] = v}
@@ -24,8 +24,8 @@ class Argument
     @twitterid = options[:twitterid]
     @sname = options[:sname]
     @tweetid = options[:tweetid]
-    options[:text] ||= ARGV[0]
-    @text = options[:text]
+    options[:posttext] ||= ARGV[0]
+    @posttext = options[:posttext]
     @image = options[:image]
     @fetchcount = options[:fetchcount]
     @fav = options[:fav]
